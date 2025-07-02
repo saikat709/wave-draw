19e2f2ad67bc55d26d7a7dfc833a84eaee5b5041
Mohammad Saikat Islam <saikatislam709@gmail.com>
2025-07-03 01:13:24 +0600

    button selection works

--- main.py
+++ main.py
@@ -6,28 +6,20 @@ from drawing import Drawing
 from ui import draw_white_header, draw_buttons, draw_color_selector, add_text
 
 
-def on_mouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(f"🖱️ Click at: ({x}, {y})")
+hand_detector = HandDetector()
+drawing = Drawing()
+
+current_draw    = DRAW_POINT
+selected_color  = COLOR_GREEN_BGR
+show_finger_tip = False
+selected_btn_id = BUTTON_PEN
 
 
-def main():
-
-    cv2.namedWindow(WINDOW_NAME)
-    cv2.setMouseCallback(WINDOW_NAME, on_mouse)
-    cap = cv2.VideoCapture(0)
-
-    hand_detector = HandDetector()
-    drawing = Drawing()
-
-    clicked_btn     = None
-    current_gesture = None
-    current_draw    = DRAW_POINT
-    selected_color  = COLOR_GREEN_BGR
-    show_finger_tip = False
+def on_tools_selected(btn_id):
+        global selected_btn_id, current_gesture, current_draw
+        selected_btn_id = btn_id
 
 
-    def on_tools_selected(btn_id):
         if btn_id == BUTTON_PEN:
             current_draw = DRAW_POINT
         if btn_id == BUTTON_COLOR:
@@ -36,6 +28,18 @@ def main():
             current_gesture = GESTURE_SELECT
         if btn_id == BUTTON_SHAPE:
             current_gesture = GESTURE_SELECT
+
+
+def on_mouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(f"Mouse click at: ({x}, {y})")
+
+
+def main():
+
+    cv2.namedWindow(WINDOW_NAME)
+    cv2.setMouseCallback(WINDOW_NAME, on_mouse)
+    cap = cv2.VideoCapture(0)
 
 
     while cap.isOpened():
@@ -58,20 +62,26 @@ def main():
             if hand_detector.is_thumbs_up():
                 add_text(image, "Thumbs Up", tx, ty)
                 current_gesture = GESTURE_SAVE
+                drawing.last_point = None
 
             elif hand_detector.hand_spread():
 
                 add_text(image, "Spread Hand", tx, ty)
                 current_gesture = None
+                drawing.last_point = None
+
 
             elif hand_detector.hand_closed():
                 add_text(image, "Hand Closed", tx, ty)
                 current_gesture = None
+                drawing.last_point = None
+
 
             elif hand_detector.tool_selection_mood():
                 show_finger_tip = True
                 add_text(image, "Tool Selection Mode", tx, ty)
                 current_gesture = GESTURE_SELECT
+                drawing.last_point = None
 
             elif hand_detector.drawing_mood():
                 
@@ -85,6 +95,7 @@ def main():
                 drawing.add_point(center[0], center[1])
 
             else:
+                drawing.last_point = None
                 add_text(image, "Unknown gesture ", tx, ty)
 
         else:
@@ -95,16 +106,6 @@ def main():
         image = drawing.draw(image)
         # draw ui
         # draw_white_header(image)
-        if current_gesture == GESTURE_SELECT:
-            center = hand_detector.find_index_tip_position()
-            draw_buttons(image, BUTTONS, top=10, 
-                         finger_tip_position=center, 
-                         on_select=lambda x: print(x)
-            )
-        else:
-            draw_buttons(image, BUTTONS, top=10)
-
-        draw_color_selector(image, COLORS, top=85)
 
         if show_finger_tip:
             center = hand_detector.find_index_tip_position()
@@ -114,13 +115,29 @@ def main():
                        thickness=cv2.FILLED
                     )
 
+        if current_gesture == GESTURE_SELECT:
+            center = hand_detector.find_index_tip_position()
+            draw_buttons(image, BUTTONS, top=10,  selected_btn_id = selected_btn_id,
+                         finger_tip_position = center, 
+                         on_select = lambda x: on_tools_selected(x),
+            )
+        else:
+            draw_buttons(image, BUTTONS, selected_btn_id = selected_btn_id, top=10)
+
+        draw_color_selector(image, COLORS, top=85)
+
+
 
         # showing
         cv2.imshow(WINDOW_NAME, image)
         key = cv2.waitKey(1)
 
         # Quit on 'q', ESC, or window close button
-        if key & 0xFF == ord('q') or key == 27 or cv2.getWindowProperty(WINDOW_NAME, cv2.WND_PROP_VISIBLE) < 1:
+        if ( 
+            key & 0xFF == ord('q') 
+            or key == 27 
+            or cv2.getWindowProperty(WINDOW_NAME, cv2.WND_PROP_VISIBLE) < 1 
+        ):
             break
 
     hand_detector.close()
--- ui.py
+++ ui.py
@@ -20,9 +20,8 @@ def draw_white_header(img, height=70):
     cv2.putText(img, text, (text_x, text_y), font, font_scale, (0, 0, 0), font_thickness, cv2.LINE_AA)
 
 
-def draw_buttons(img, buttons, finger_tip_position = None, on_select = None, 
-                 top=80, button_height=60, button_width=120, gap=30, 
-                 color=(220, 220, 220), text_color=(0, 255, 0)
+def draw_buttons(img, buttons, finger_tip_position = None, on_select = None, selected_btn_id=None, 
+                 top = 80, button_height=60, button_width=120, gap=30, 
                 ):
     total_width = len(buttons) * button_width + (len(buttons) - 1) * gap
     start_x = (img.shape[1] - total_width) // 2
@@ -38,33 +37,41 @@ def draw_buttons(img, buttons, finger_tip_position = None, on_select = None,
         y2 = y1 + button_height
 
         if finger_tip_position and on_select and x1 <= finger_tip_position[0] <= x2 and y1 <= finger_tip_position[1] <= y2:
-            print(f"Button {button['text']} selected")
             on_select(button["id"])
 
-        cv2.rectangle(img, (x1, y1), (x2, y2), color, 0)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)  # border
+        selected   = selected_btn_id == button["id"]
+        thickness  = -1 if selected else 2
+        color      = (0, 255, 0) 
+        text_color = (255, 255, 255) if selected else (0, 255, 0 )
+
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), thickness)  # border
 
         text_size, _ = cv2.getTextSize(button["text"], font, font_scale, font_thickness)
         text_x = x1 + (button_width - text_size[0]) // 2
         text_y = y1 + (button_height + text_size[1]) // 2
 
-        cv2.putText(img, button["text"], (text_x, text_y), font, font_scale, text_color, font_thickness, cv2.LINE_AA)
+        cv2.putText(img, button["text"], 
+                    (text_x, text_y), 
+                    font, font_scale, text_color, 
+                    font_thickness, cv2.LINE_AA
+                )
 
 
-def draw_color_selector(img, colors, top=150, size=40, gap=20, selected_index=None):
+def draw_color_selector(img, colors, top=150, size=60, gap=40, selected_index=None):
     total_width = len(colors) * size + (len(colors) - 1) * gap
     start_x = (img.shape[1] - total_width) // 2
 
     for i, clr in enumerate(colors):
         x1 = start_x + i * (size + gap)
         y1 = top
-        x2 = x1 + size
+        x2 = x1 + size + 30
         y2 = y1 + size
 
         cv2.rectangle(img, (x1, y1), (x2, y2), clr, -1)
 
         border_color = (0, 0, 0) if selected_index != i else (0, 255, 0)
         cv2.rectangle(img, (x1, y1), (x2, y2), border_color, 2)
+
 
 
 def add_text(img, text, x, y, text_color=(0, 255, 0)):
